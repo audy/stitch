e9b6bf0586909c2df256d47a3774f2581cb865df
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-06-14 12:31:19 -0400

    make it work

--- stitch/stitch.py
+++ stitch/stitch.py
@@ -8,12 +8,20 @@ import sys
 import time
     
 def main():
-    parser = OptionParser(description="stitch.py")
+    parser = OptionParser(
+        description="""Stitch - Tool for creating contigs from overlapping
+paried-end illumina reads.""",
+        usage='-i fastqfile1 -j fastqfile2')
     parser.add_option('-i', '--first', dest='filea')
     parser.add_option('-j', '--second', dest='fileb')
 
     (options, args) = parser.parse_args()
-
+    
+    if not (options.filea or options.fileb):
+        print >> sys.stderr, 'Usage: %s %s' % \
+            (parser.get_prog_name(), parser.usage)
+        quit()
+    
     seqsa = open(options.filea, 'r')
     seqsb = open(options.fileb, 'r')
     
@@ -32,7 +40,7 @@ def main():
 def doStitch(recs):
     reca, recb = recs
     return Stitch(reca, recb)
-        
+
 class Stitch:
     def __init__(self, reca, recb):
         self.reca = reca
@@ -42,74 +50,23 @@ class Stitch:
         if result:
             for key in result:
                 setattr(self, key, result[key])
-            self.generate_contig()
-    def generate_contig(self): 
-        # i am the shittiest programmer in the world!
+            self._generate_contig()
+            
+    def _generate_contig(self): 
         if self.qstart < self.sstart:
             self.hits = True
-            subject, subqual = self.reca.seq, self.reca.qual
-            query, quequal = self.recb.revcomp, self.recb.qual
-            qs, ss, qe, se = self.qstart, self.sstart, self.qend, self.send
-            print qs, ss, qe, se
+            subject, subqual = self.recb.seq, self.recb.qual
+            query, quequal = self.reca.revcomp, self.reca.qual[::-1]
             
-            # first region, comes from query sequence
-            a = subject[0:ss]
-            aq = subject[0:ss]
-
-            # vote (overlap) region, all should be the same length
-            bqs = query[0:len(subject)-ss+qs]
-            bqq = quequal[0:len(subject)-ss+qs]
-            bss = subject[ss-qs:]
-            bsq = subqual[ss-qs:]
             
-            b = []
-            bq = []
             
-            # seriously, someone shoot me.
-            for ((qn, qq), (sn, sq)) in izip(izip(bqs, bqq), izip(bss, 0bsq)):
-                if qq > sq:
-                    b += qn
-                    bq += qq
-                elif qq < sq:
-                    b += sn
-                    bq += sq
-                elif qq == sq:
-                    if qn == sn:
-                        b += sn
-                        bq += sq
-                    elif qn != sn:
-                        if qn == 'N':
-                            b += sn
-                            bq += sq
-                        elif sn == 'N':
-                            b += qn
-                            bq += qq
-                        if (qn and sn) in ('A', 'G'):
-                            b += 'R'
-                            bq += max(qq, sq)
-                        elif (qn and sn) in ('T', 'C'):
-                            b += 'Y'
-                            bq += max(qq, sq)
-                        elif (qn and sn) in ('A', 'C'):
-                            b += 'M'
-                            bq += max(qq, sq)
-                        elif (qn and sn) in ('G', 'C'):
-                            b += 'S'
-                            bq += max(qq, sq)
-                        elif (qn and sn) in ('A', 'T'):
-                            b += 'W'
-                            bq += max(qq, sq)
-                        else:
-                            b += 'N'
-                            bq += max(qq, sq)
-                                        
-            b = ''.join(b)
+            for nuc in subject[]
             
-            # last region, comes from subject sequence
-            c = query[len(subject)-qs+ss:]
-            cq = quequal[len(subject)-qs+ss:]
+            finalsec, finalqual = [], []
             
-            setattr(self, 'contig', a+b+c)
+            contig = { "seq": finalseq, "qual": finalqual}
+           
+            setattr(self, 'contig', contig)
             
             
             
