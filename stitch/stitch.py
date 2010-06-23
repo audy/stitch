97be32c6a9905abc4159a7e0d9e303ccaf90a923
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-06-23 18:49:09 -0400

    figured it out and it works, now on to output stuff

--- stitch/stitch.py
+++ stitch/stitch.py
@@ -19,51 +19,75 @@ paried-end illumina reads.""",
 
     (options, args) = parser.parse_args()
     
-    if not (options.filea and options.fileb and options.prefix):
+    if not (options.filea and options.fileb):
         print >> sys.stderr, 'Usage: %s %s' % \
             (parser.get_prog_name(), parser.usage)
         sys.exit()
-    
+    if not (options.prefix):
+        print >> sys.stderr, 'Warning: no outputfile'
+        dudsa, dudsb, outfile = sys.stdout, sys.stdout, sys.stdout
+    else:
+        dudsa = open('%s-nh-s1.fastq' % options.prefix, 'w')
+        dudsb = open('%s-nh-s2.fastq' % options.prefix, 'w')
+        outfile = open('%s-contigs.fastq' % options.prefix , 'w')
+
     seqsa = open(options.filea, 'r')
     seqsb = open(options.fileb, 'r')
     
     numcontigs, numtotes = 0, 0
     
-    dudsa = open('%s-nh-s1.fastq' % options.prefix, 'w')
-    dudsb = open('%s-nh-s2.fastq' % options.prefix, 'w')
-    outfile = open('%s-contigs.fastq' % options.prefix , 'w')
-    
     p = Pool()
      
-    for i in imap(doStitch, izip(Fasta(seqsa), Fasta(seqsb))):
-        if i.hits:
-            print >> outfile, i.record
+    for i in p.imap(doStitch, izip(Fasta(seqsa), Fasta(seqsb))):
+        numtotes += 1
+        if i.record:
             numcontigs += 1
         else:
-            pass
-        numtotes += 1
-    dudsa.close()
-    dudsb.close()
-    outfile.close()
+            print 'loser!'
+        
     
     print 'Made %s contigs, out of %s reads' % \
         (numcontigs, numcontigs + numtotes)
-    
+        
     
 class Stitch:
     ''' Stitches together two overlapping Illumina reads using Doubleblast '''
     def __init__(self, reca, recb):
         self.reca = reca
         self.recb = recb
-        self.hits = False
+        self.record = False
         self.contig = []
         self.quality = []
-        result = self.find_overlaps()
+        self.find_overlaps()
         
     def find_overlaps(self):
         ''' Alignment algorithm, returns new DNA object of contig '''
-        print self.reca.seq, self.recb.seq
-        raise StopIteration
+        seqa, seqb = self.reca.seq, self.recb.revcomp
+        seqb = self.recb.revcomp
+        
+        length = min([len(i) for i in (seqa, seqb)])
+        scores = []
+        
+        hits = {}
+        
+        for i in range(len(seqa)):
+            score = 0
+            for na, nb in zip(seqa[i-1:], seqb[:-i+1]):
+                if na == nb:
+                    score += 1
+            hits[score] = i
+        
+        
+        score = max(hits.keys())
+        i = hits[score]
+        
+        if ((score > 20)):
+            print 'winner! %s' % (i)
+            print seqa + '-'*(i-1)
+            print '-'*(i-1) + seqb
+            self.record = self.reca
+
+
 
 def doStitch(recs):
     ''' Used by Pool.imap to create stitch jobs '''
