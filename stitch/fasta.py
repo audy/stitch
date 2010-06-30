f3da269d01cb8d9434e1acd63253a521c4efebaa
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-06-30 16:37:22 -0400

    dna->fasta.py

--- stitch/fasta.py
+++ stitch/fasta.py
@@ -1,4 +1,5 @@
-from dna import *
+import string
+_complement = string.maketrans('GATCRYgatcry','CTAGYRctagyr')
 
 class Fasta:
     ''' iterates through a fasta or fastq file, returning dnaobj objects '''
@@ -21,3 +22,38 @@ class Fasta:
         elif self.filetype == 'fasta':
             # implement this later
             pass
+
+class Dna:
+    ''' An object representing either a FASTA or FASTQ record '''
+    def __init__(self, header, sequence, quality = False):
+        self.header = header[1:-1]
+        self.seq = sequence
+        self.qual = quality
+        if quality:
+            self.type = 'fastq'
+        else:
+            self.type = 'fasta'
+    def __str__(self):
+        ''' returns a FASTA/Q formatted string '''
+        if not self.qual:
+            return ('>%s\nself.sequence\n') % \
+                (self.header, self.seq)
+        else:
+            return('\n@%s\n%s\n+%s\n%s') % \
+                (self.header, self.seq, self.header, self.qual)
+    def __len__(self):
+        return len(self.seq)
+    def __repr__(self):
+        return '<dnaobj.%s instance: %s>' % (self.type, self.header)
+    @property
+    def complement(self):
+        ''' returns complement of sequence '''
+        return self.seq.translate(_complement)
+    @property 
+    def revcomp(self):
+        ''' returns reverse complement of sequence '''
+        return self.complement[::-1]
+    @property
+    def rqual(self):
+        ''' returns reverse quality'''
+        return self.qual[::-1]
