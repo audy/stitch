6e7db00069a9527a4f27f5eaa8f936519b570f99
Austin G. Davis-Richardson <harekrishna@gmail.com>
2010-07-27 13:38:59 -0400

    filters reads by length - useful preprocessing step for assembly

---
+++ stitch/length_filter.py
@@ -0,0 +1,17 @@
+from fasta import *
+import sys
+
+filename = sys.argv[1]
+
+for minimum, maximum in ((50, 59), (60, 69), (70, 79), (80, 89), (90, 99), (100, 129), (130, 200)):
+    handle = open(filename)
+    output = open('%s-%s-%s' % (filename, minimum, maximum), 'w')
+    counter = 0 
+    for fasta in Fasta(handle):
+        if len(fasta) > maximum:
+            continue
+        if len(fasta) < minimum:
+            continue
+        print >> output, fasta
+        counter += 1
+    print minimum, maximum, counter
