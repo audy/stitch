c5acd58cd1c0198520be4d948fa23b8f8eb2bc73
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-07-20 13:01:06 -0400

    this project needs testing, bad

---
+++ test/stitch.py
@@ -0,0 +1,11 @@
+def test_stitch():
+    ''' make sure stitch algorithm behaves rationally '''
+    pass
+    
+def test_dnaio():
+    ''' make sure fastq and fasta parsing/output behave rationally '''
+    pass
+    
+assert test_stitch()
+
+assert test_dnaio()
