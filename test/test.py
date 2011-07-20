53d5e3c82c7675cfe66fa3af0632ecf56fdd4c72
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-07-20 17:16:32 -0400

    using unittest framework now

--- test/test.py
+++ test/test.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 
 from stitch import *
+import unittest
 
-def test_stitch():
-    ''' make sure stitch algorithm behaves rationally '''
+
+class TestDnaioFunctions(unittest.TestCase):
+    ''' test DNA IO '''
+    def test_headers(self):
+        ''' make sure headers are parsed correctly '''
+        with open('seq_a.txt') as handle:
+            records = [i for i in Fasta(handle)]
+            
+        self.assertEqual(records.pop(0).header, 'sequence_1')
+
+
+class TestAlignments(unittest.TestCase):
+    ''' test alignment functions for rationality '''
     
-    ha = open('seq_a.txt')
-    hb = open('seq_b.txt')
-    
-    records = ((a, b) for (a, b) in zip(Fasta(ha), Fasta(hb)))
-    contigs = (i.strip() for i in open('contigs.txt').readlines())
-    for i, j in records:
-        c = contigs.next()
-        res = Stitch(i, j).record.seq
-        assert res == c
-    
-    return True
-    
-def test_dnaio():
-    ''' make sure fastq and fasta parsing/output behave rationally '''
-    return True
-    
-def run_tests():
-    assert test_stitch()
-    assert test_dnaio()
-    
-run_tests()
+    def test_alignments(self):        
+        with open('seq_a.txt') as ha, open('seq_b.txt') as hb:
+            records = [(a, b) for (a, b) in zip(Fasta(ha), Fasta(hb))]
+
+        contigs = [ i.strip() for i in open('contigs.txt').readlines() ]
+
+        for i, j in records:
+            c = contigs.pop(0)
+            res = Stitch(i, j).record.seq
+            self.assertEqual(res, c)
+
+
+if __name__ == '__main__':
+    unittest.main()
