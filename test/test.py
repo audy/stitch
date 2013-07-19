9e8631978c7da4257ea3e7d3ee2d0d44a36869e7
audy <harekrishna@gmail.com>
2013-07-19 16:14:00 -0400

    whitespace

--- test/test.py
+++ test/test.py
@@ -6,10 +6,10 @@ def test_headers():
     ''' make sure headers are parsed correctly '''
     with open('seq_a.txt') as handle:
         records = [i for i in Fasta(handle)]
-        
+
     assert records.pop(0).header == 'sequence_1'
 
-def test_alignments():        
+def test_alignments():
     with open('seq_a.txt') as ha, open('seq_b.txt') as hb:
         records = [(a, b) for (a, b) in zip(Fasta(ha), Fasta(hb))]
 
@@ -18,8 +18,8 @@ def test_alignments():
     for i, j in records:
         c = contigs.pop(0)
         res = Stitch(i, j)
-        
-        # TODO, also check quality scores 
+
+        # TODO, also check quality scores
         # print res.pretty
         seq = res.record.seq
         try:
